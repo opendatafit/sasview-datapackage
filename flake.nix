10f62096dfb11e33a1cef93c4d15151090abca3c
echus <varvara@echus.co>
2023-10-24 11:51:06 +1100

    Initial commit with flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "";
+
+  # Use the unstable nixpkgs to use the latest set of node packages
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.python310
+          pkgs.python310Packages.numpy
+          pkgs.python310Packages.scipy
+          pkgs.python310Packages.pandas
+        ];
+      };
+    });
+}
